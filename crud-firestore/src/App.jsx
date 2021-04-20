e4d39d9dec9d629f6758a6d6c53b0a75163f1bf5
German <germandenaroc@gmail.com>
2021-04-20 18:14:58 -0300

    Se agredo funcion editar y eliminar

--- crud-firestore/src/App.jsx
+++ crud-firestore/src/App.jsx
@@ -4,6 +4,11 @@ import {firebase} from './firebase'
 
 function App() {
 
+  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState('');
+  const [tareida, setId] = useState('');
+  const [modoEdicion, setModoEdicion] = useState(false);
+
   useEffect(() => {
 
     const obtenerDatos = async () => {
@@ -12,8 +17,9 @@ function App() {
         
           const db = firebase.firestore();
           const data = await db.collection('tareas').get();
-          console.log(data.docs);
-
+          const arrayData = data.docs.map(doc => ({id: doc.id, ...doc.data()}))
+          console.log(arrayData);
+          setTareas(arrayData)
         } catch (error) {
           console.log(error);
         }
@@ -21,9 +27,110 @@ function App() {
     obtenerDatos()
   }, [])
 
+  const agregar = async (e) => {
+    e.preventDefault();
+
+    if(!tarea.trim) {
+      console.log('Esta vacio');
+      return
+    }
+
+    try {
+
+      const db = firebase.firestore();
+      const nuevaTarea = {
+        name: tarea,
+        fecha: Date.now()
+      }
+      const data = await db.collection('tareas').add(nuevaTarea);
+      setTareas([
+        ...tareas,
+        {...nuevaTarea, id: data.id}
+      ]);
+
+      setTarea('');
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const eliminar = async (id) => {
+    try {
+      
+      const db = firebase.firestore()
+      await db.collection('tareas').doc(id).delete();
+
+      const arrayFiltrado = tareas.filter(item => item.id !== id)
+      setTareas(arrayFiltrado)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const activarEdicion = (item) => {
+    setModoEdicion(true)
+    setTarea(item.name)
+    setId(item.id)
+  }
+
+  const editar = async (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="container">
-      asdsa
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-6">
+          <ul className="list-group">
+            {
+              tareas.map(item => (
+                <li className="list-group-item" key={item.id}>
+                  {item.name}
+                  <button 
+                    className="btn btn-danger btn-sm float-right"
+                    onClick={() => eliminar(item.id)}>
+                    Eliminar
+                  </button>
+                  <button 
+                    className="btn btn-warning btn-sm float-right mr-2"
+                    onClick={() => activarEdicion(item)}
+                  >
+                    Editar
+                  </button>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="cold-md-6">
+          <h3>
+            {
+              modoEdicion ? 'Editar Tarea' : 'Agregar Tarea'
+            }
+          </h3>
+          <form onSubmit={modoEdicion ? editar : agregar}>
+            <input 
+              type="text" 
+              placeholder='Ingrese Tarea' 
+              className='form-control mb-2' 
+              onChange={e => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <button 
+              className={
+                modoEdicion ? 'btn btn-warning btn-block' : 'btn btn-dark btn-block'
+              }
+              type='submit'
+              >     
+                {
+                  modoEdicion ? 'Editar' : 'Agregar'
+                }
+              </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }

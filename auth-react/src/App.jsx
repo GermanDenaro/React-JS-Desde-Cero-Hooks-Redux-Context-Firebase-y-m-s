f3235ae5681d774098811afb542aab6bcac287f2
German <germandenaroc@gmail.com>
2021-04-22 18:06:45 -0300

    Creado proyecto auth

---
+++ auth-react/src/App.jsx
@@ -0,0 +1,30 @@
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Navbar/>
+      <Switch>
+        <Route exact path='/'>
+          inicio...
+        </Route>
+        <Route path='/login'>
+          <Login/>
+        </Route>
+        <Route path='/admin'>
+          admin...
+        </Route>
+        
+      </Switch>
+    </div>
+    </Router>
+   
+  );
+}
+
+export default App;
---
+++ auth-react/src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCuLfbLwAB2QqW6JD3Zqyg_fg2-M2wggDQ",
+    authDomain: "crud-firestore-a561f.firebaseapp.com",
+    projectId: "crud-firestore-a561f",
+    storageBucket: "crud-firestore-a561f.appspot.com",
+    messagingSenderId: "622700538688",
+    appId: "1:622700538688:web:267e4884c9f15b3026a8a5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};
